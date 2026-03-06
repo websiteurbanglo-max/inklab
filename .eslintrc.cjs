71dc277bade91e13e283d267c5ba87545e204863
Manasbits <f20220784@goa.bits-pilani.ac.in>
2026-03-06 15:10:28 +0530

    Fixed lint errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -84,6 +84,7 @@ module.exports = {
         ".graphqlrc.{js,ts}",
         "shopify.server.{js,ts}",
         "**/*.server.{js,ts}",
+        "app/routes/**/*.{js,jsx,ts,tsx}",
       ],
       env: {
         node: true,
